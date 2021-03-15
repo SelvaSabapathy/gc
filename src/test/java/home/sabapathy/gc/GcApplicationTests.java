fe06213a174f82259dd662c56f7392ac5d359b9e
Selva Sabapathy <selva.sabapathy@gmail.com>
2021-03-15 19:05:33 -0400

    Initial commit

---
+++ src/test/java/home/sabapathy/gc/GcApplicationTests.java
@@ -0,0 +1,13 @@
+package home.sabapathy.gc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
